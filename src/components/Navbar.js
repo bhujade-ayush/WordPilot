da777e229664b9a8403bb1b6083836ca9eba05d1
Ayush Bhujade <bhujadeayush06@gmail.com>
2025-06-10 14:43:18 +0530

    Home Page Updated

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/Navbar.css";
+export default function Navbar(props) {
+  return (
+    <header>
+      <a href="/" className="logo">
+        {props.title}
+      </a>
+      <nav>
+        <ul className="navLinks">
+          <li className="Home">
+            <a href="/">Home</a>
+          </li>
+          <li className="Projects">
+            <a href="/">Projects</a>
+          </li>
+          <li className="About">
+            <a href="/">About</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="btns">
+        <a href="/">
+          <button>Log in</button>
+        </a>
+        <a href="/">
+          <button>Register</button>
+        </a>
+      </div>
+    </header>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomeLayout from "../layouts/HomeLayout";
+
+const Home = () =>{
+    return (
+        <>
+        <HomeLayout>
+
+        </HomeLayout>
+        </>
+    );
+};
+
+export default Home;
