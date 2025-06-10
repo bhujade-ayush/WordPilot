08b53e917c3ddc8338ed9bde455b4d9733fe661a
Ayush Bhujade <bhujadeayush06@gmail.com>
2025-06-10 16:44:58 +0530

    Register button added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
 import React from "react";
 import HomeLayout from "../layouts/HomeLayout";
-import "../styles/Home.css"
-import hero from "../assets/images/hero.png"
+import "../styles/Home.css";
+import hero from "../assets/images/hero.png";
 
 const Home = () => {
   return (
     <>
       <HomeLayout>
         <div className="hero">
-            <div className="left">
-                <h1>Minimal Workspace, Maximum Control</h1>
-                <p>Clean interface. Smooth workflow. Everything you need, nothing you don't.</p>
-            </div>
-            <div className="right">
-                <img src={hero} alt="" />
-            </div>
+          <div className="left">
+            <h1>Minimal Workspace, Maximum Control</h1>
+            <p>
+              Clean interface. Smooth workflow. Everything you need, nothing you
+              don't.
+            </p>
+            <a href="/" className="register">
+              <button>Register</button>
+            </a>
+          </div>
+          <div className="right">
+            <img src={hero} alt="" />
+          </div>
         </div>
       </HomeLayout>
     </>
